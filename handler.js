5b28c4aa6523552d5bf28ff860bf5ce80730aee5
Mert <57871683+mekaan@users.noreply.github.com>
2021-06-12 19:00:09 +0300

    import problem and error handling problem solved

--- handler.js
+++ handler.js
@@ -1,45 +1,31 @@
-'use strict';
-import { success, failure } from "libs/response-lib";
+import { success, failure } from "./libs/response-lib";
+import AWS from "aws-sdk";
 const client = new AWS.DynamoDB.DocumentClient();
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
 
 export async function add (event, context) {
   try{
     //event body -> id, name, surname
-    const data = event.body
+    const data = event.body;
 
-    if (!('phoneNumber' in data)) {
-      return failure({status: false, error: "Phone number is required for creation."});
-    }else if(data.email=="") {
-      return failure({status: false, error: "Please enter phone number."});
+    if (!('id' in data)) {
+      return failure({status: false, error: "ID is required for creation."});
+    }else if(data.id=="") {
+      return failure({status: false, error: "Please enter ID."});
     }
 
-    const date = new Date()
-    data.created_at = date.toISOString()
-    data.updated_at = date.toISOString()
+    const date = new Date();
+    data.created_at = date.toISOString();
+    data.updated_at = date.toISOString();
 
     const params = {
       TableName: process.env.tableName,
       Item: data
-    }
+    };
 
-    return client.transactWrite(params).promise()
+    await client.put(params).promise();
+
+    return success(data);
 
   } catch (e) {
     return failure({status: false, error: e.message});
@@ -55,14 +41,14 @@ export async function get (event, context) {
       },
     };
 
-    const data = client.get(params).promise()
+    const data = await client.get(params).promise();
     if(data.Item) {
       return success(data.Item);
     } else {
-      return failure({status: false, error : "Student do not exists!"})
+      return failure({status: false, error : "Student do not exists!"});
     }
 
   } catch (e){
-    return failure({status: false, error: e.message})
+    return failure({status: false, error: e.message});
   }
 }
